2eaeda59b4ae0c5ddc459179063fc868a144a537
ValentinCamus <ad.valentin.camus@gmail.com>
2020-02-10 23:28:07 +0100

    Refactoring (TextureLoader)

--- Sources/IO/Loader/TextureLoader.cpp
+++ Sources/IO/Loader/TextureLoader.cpp
@@ -4,36 +4,21 @@
 
 namespace gir
 {
-    Texture2D *TextureLoader::Load(const std::string &filename, bool flipYAxis)
+    Texture2D* TextureLoader::Load(const std::string &filename, bool flipYAxis)
     {
-        stbi_set_flip_vertically_on_load(flipYAxis);
-
-        int width = 0, height = 0, channels = 0;
-        const char * cPath = filename.c_str();
-
         Logger::Info("Loading image \"{0}\"...", filename);
 
-        const unsigned char* pixels = stbi_load(cPath, &width, &height, &channels, 0);
+        stbi_set_flip_vertically_on_load(flipYAxis);
+
+        int width = 0;
+        int height = 0;
+        int nChannels = 0;
+        const char * cPath = filename.c_str();
+
+        const unsigned char* pixels = stbi_load(cPath, &width, &height, &nChannels, 0);
         GIR_ASSERT(pixels, "TextureLoader::Load: Image not found");
 
-        int format;
-        switch (channels)
-        {
-            case 1:
-                format = GL_RED;
-                break;
-            case 2:
-                format = GL_RG;
-                break;
-            case 3:
-                format = GL_RGB;
-                break;
-            default:
-                format = GL_RGBA;
-                break;
-        }
-
-        auto* texture = new Texture2D(filename, format, GL_UNSIGNED_BYTE);
+        auto* texture = new Texture2D(filename, GetFormat(nChannels), GL_UNSIGNED_BYTE);
         texture->Bind(0);
         texture->Allocate(width, height, pixels);
         texture->Unbind();
@@ -43,14 +28,16 @@ namespace gir
         return texture;
     }
 
-    template<typename Callback>
-    void TextureLoader::AsyncLoad(const std::string &filename, Callback callback)
+    int TextureLoader::GetFormat(unsigned nChannels)
     {
+        GIR_ASSERT(nChannels <= 4, "TextureLoader: More than 4 channels is not supported");
 
-    }
-
-    void TextureLoader::Wait()
-    {
-
+        switch (nChannels)
+        {
+            case 1: return GL_RED;
+            case 2: return GL_RG;
+            case 3: return GL_RGB;
+            default: return GL_RGBA;
+        }
     }
 }
--- Sources/IO/Loader/TextureLoader.hpp
+++ Sources/IO/Loader/TextureLoader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <future>
+
 #include <Core/Core.hpp>
 
 #include <Engine/Texture/Texture2D.hpp>
@@ -11,10 +13,8 @@ namespace gir
     public:
         static Texture2D* Load(const std::string& filename, bool flipYAxis = true);
 
-        template<typename Callback>
-        static void AsyncLoad(const std::string& filename, Callback callback);
-
-        static void Wait();
+    private:
+        static int GetFormat(unsigned nChannels);
     };
 }
 
